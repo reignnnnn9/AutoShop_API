de78dd88c73766ca313d367f6842789c547675d7
reignnnnn9 <reigne50@gmail.com>
2025-09-24 17:58:33 -0500

    Added customer documentation

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,18 @@ from .extensions import ma, limiter, cache
 from .blueprints.customers import customer_bp
 from .blueprints.mechanics import mechanic_bp
 from .blueprints.service_tickets import ticket_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yaml'  # Our API URL (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Mechanic API"
+    }
+)
 
 def create_app(config_name):
 
@@ -20,5 +32,6 @@ def create_app(config_name):
     app.register_blueprint(customer_bp, url_prefix='/customers')
     app.register_blueprint(mechanic_bp, url_prefix='/mechanics')
     app.register_blueprint(ticket_bp, url_prefix='/service_tickets')
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL) #Registering our swagger blueprint
 
     return app
